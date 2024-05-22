15aa6029c42178c8523234080a08dc3939437dcb
Niko Niemelä <nokka200@gmail.com>
2024-05-22 11:18:42 +0300

    Lisäys

--- Calculator/Class1.cs
+++ Calculator/Class1.cs
@@ -2,8 +2,21 @@
 
 static public class Calculator
 {
+    /// <summary>
+    /// Tämä metodi lisää kaksi numeroa ja palauttaa aina positiivisen tuloksen.
+    /// </summary>
+    /// <param name="first">Ensimmäinen numero</param>
+    /// <param name="second">Toinen numero</param>
+    /// <returns>Positiivinen arvo</returns>
     static public int Adder(int first, int second)
     {
         return first + second;
     }
+
+    /*
+    static public int Adder(int first, int second)
+    {
+        return Math.Abs(first + second);
+    }
+    */
 }
--- CalculatorTest/UnitTest1.cs
+++ CalculatorTest/UnitTest1.cs
@@ -18,4 +18,19 @@ public class Calculator_Adder
         // Assert
         Assert.AreEqual(compare, result);
     }
+
+    [TestMethod]
+    [DataRow(-1, -2, DisplayName = "-1 + -2 = 3")]
+    [DataRow(-2, -3, DisplayName = "-2 + -3 = 5")]
+    public void Adder_IntTwoNegativeNumbers_ReturnTrue(int first, int second)
+    {
+        // Arrange
+        int compare = Math.Abs(first + second);
+
+        // Act
+        var result = Calculator.Adder(first, second);
+
+        // Assert
+        Assert.AreEqual(compare, result);
+    }
 }
